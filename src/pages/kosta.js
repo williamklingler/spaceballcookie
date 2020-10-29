b35b094b707fd80fe5b2cd96ac54e10332358491
gianicok <kostagianicos@gmail.com>
2020-10-29 13:46:23 -0400

    added kosta page

---
+++ src/pages/kosta.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Particles from "react-tsparticles";
+import options from '../graphics/particleOptionsKosta.js'
+
+
+export default class Kosta extends React.Component{
+  render(){
+    return(
+      <div>
+        <title>Kosta Gianicos 💚</title>
+        <Particles height="100vh" options={options} />
+        <div id="willHeader"> KOSTA GIANICOS </div>
+      </div>
+    )}
+  }
