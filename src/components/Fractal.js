56a0a358c825528380afcac2949a640dc4054c0b
gianicok <kostagianicos@gmail.com>
2020-10-29 13:35:00 -0400

    testing testing 12...3

---
+++ src/components/Fractal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Fractal extends React.Component {
+  render() {
+    return (
+      <div id="fractalBackground">
+        <p>hi</p>
+      </div>
+    )}
+}
